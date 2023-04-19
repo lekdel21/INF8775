5b50ef63ebd63dd4c4aabaaae226eec3c9faff60
lekdel21 <90577310+lekdel21@users.noreply.github.com>
2023-04-19 11:05:22 -0400

    Delete transform_solution.py

--- transform_solution.py
+++
@@ -1,8 +0,0 @@
-
-def transform(solution, n):
-    new_solution = [[] for _ in range(n)]
-    for y, row in enumerate(solution):
-        for x, value in enumerate(row):
-            if value is not None:
-                new_solution[value].append((x, y))
-    return new_solution
