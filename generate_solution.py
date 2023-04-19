77b11630e29a0632223f7603027815ae82c6aeed
ghhar98 <78988892+ghhar98@users.noreply.github.com>
2023-04-19 11:33:46 -0400

    Alek changes

--- generate_solution.py
+++ generate_solution.py
@@ -1,88 +1,34 @@
-import random
-from read_input_file import read_input_file
-from helper import calculate_score
-from swap import swap_initial
+from in_out_file import read_input_file, write_solution_to_file
+import math
+import numpy as np
+from score_calculator import calculate_score
+from swap import get_enclosure_coords, initial_swap, local_search
 
-def extract_free_cardinal_point(x, y, used_coordinates, directions):
-    for dx, dy in directions:
-        nx, ny = x + dx, y + dy
-        if (nx, ny) not in used_coordinates:
-            return (nx, ny)
-    return None
-
-def generate_contiguous_enclosure(size, used_coordinates):
-    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    x_offset, y_offset = 0, 0
-    if len(used_coordinates) != 0: 
-        max_x = max(used_coordinates, key=lambda coord: coord[0])[0]
-        max_y = max(used_coordinates, key=lambda coord: coord[1])[1]
-        x_offset, y_offset = max_x + 1, max_y + 1
-        
-    
-    enclos = [(x_offset, y_offset)]
-    fringe = [(x_offset, y_offset)]
-    used_coordinates.add((x_offset, y_offset))
-
-    while len(enclos) < size:
-        x, y = fringe.pop(0)
-        random.shuffle(directions)
-        found = False
-        for dx, dy in directions:
-            nx, ny = x + dx, y + dy
-            if (nx, ny) not in used_coordinates:
-                enclos.append((nx, ny))
-                used_coordinates.add((nx, ny))
-                fringe.append((nx, ny))
-                found = True
+def generate_initiale_solution(enclos_sizes, enclos_weights):
+    n = len(enclos_sizes)
+    size = math.ceil(math.sqrt(n))
+    solution = [[]] * n
+    enclos_number = 0
+    for j in range(size):
+        for i in range(size):
+            if(enclos_number >= n): 
                 break
-
-        if not found:
-            # Go back to the first coordinate and check if any of the cardinal points are free
-            x, y = enclos[0]
-            cardinal_point = extract_free_cardinal_point(x, y, used_coordinates, directions)
-            if cardinal_point is not None:
-                enclos.append(cardinal_point)
-                used_coordinates.add(cardinal_point)
-                fringe.append(cardinal_point)
-                found = True
-            if not found:
-                raise RuntimeError("Failed to generate a contiguous enclosure")
-
-    return enclos
-
-
-def generate_random_solution(enclos_sizes):
-    used_coordinates = set()
-    solution = []
-
-    for size in enclos_sizes:
-        enclos = generate_contiguous_enclosure(size, used_coordinates)
-        solution.append(enclos)
-
+            else:
+                solution[enclos_number] = get_enclosure_coords(enclos_sizes[enclos_number], (i*5, j*4))
+            enclos_number += 1   
     return solution
 
-def write_solution_to_file(solution, file_path):
-    with open(file_path, 'w') as file:
-        for enclos in solution:
-            coordinates = ' '.join(f"{int(x)} {int(y)}" for x, y in enclos)
-            file.write(coordinates + '\n')
 
 # Read input file
 input_file_path = "n20_m15_V-74779.txt"
-n, m, k, enclos_bonus, enclos_sizes, enclos_weights = read_input_file(input_file_path)
+n, m, k, subset, enclos_sizes, enclos_weights = read_input_file(input_file_path)
 
-# Generate random solution
-random_solution = generate_random_solution(enclos_sizes)
-score = calculate_score(random_solution, enclos_weights, enclos_bonus, k)
+solution = generate_initiale_solution(enclos_sizes, enclos_weights)
+
+score = calculate_score(solution, enclos_weights, subset, k)
 print(score)
 
-initial_solution = swap_initial(random_solution)
-score = calculate_score(initial_solution, enclos_weights, enclos_bonus, k)
-print(score)
+solution = local_search(solution, enclos_weights, subset, k, enclos_sizes, 1000)
 
-# Write solution to a file
 output_file_path = "sol_n20_m15_V-74779.txt"
-write_solution_to_file(initial_solution, output_file_path)
-
-
-
+write_solution_to_file(solution, output_file_path)
